c811c27457aba8f6e800de0c8c004b92b17ba66b
Anstasiya <ieanastasiya@mail.ru>
2023-02-10 08:12:46 +0300

    optimized the function by reducing the number of cycles

--- projects_slider.js
+++ projects_slider.js
@@ -14,7 +14,6 @@ function initSlider(options) {
 if (!images || !images.length) return;
 
 options = options || {
-  dots: true,
   autoplay: false
 };
 
@@ -23,23 +22,32 @@ let sliderArrows = document.querySelector(".slider__arrows");
 let sliderDots = document.querySelector(".slider__dots");
 let sliderTitle = document.querySelector(".galary-list");
 
-initImages();
+initImagesTitleDots();
 initArrows();
-initTitle();
-
-if (options.dots) {
-  initDots();
-}
 
 if (options.autoplay) {
   initAutoplay();
 }
 
-function initImages() {
+function initImagesTitleDots() {
   images.forEach((image, index) => {
     let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${images[index].url});" data-index="${index}"></div>`;
+    let title = `<li class="galary-item n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].title}</li>`;
+    let dot = `<div class="slider__dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
     sliderImages.innerHTML += imageDiv;
-  });
+    sliderTitle.innerHTML += title;
+    sliderDots.innerHTML += dot;
+  })
+  sliderTitle.querySelectorAll(".galary-item").forEach(title => {
+    title.addEventListener("click", function() {
+      moveSlider(this.dataset.index);
+    })
+  })
+  sliderDots.querySelectorAll(".slider__dots-item").forEach(dot => {
+    dot.addEventListener("click", function() {
+      moveSlider(this.dataset.index);
+    })
+  })
 }
 
 function initArrows() {
@@ -57,39 +65,15 @@ function initArrows() {
   });
 }
 
-function initDots() {
-  images.forEach((image, index) => {
-    let dot = `<div class="slider__dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
-    sliderDots.innerHTML += dot;
-  });
-  sliderDots.querySelectorAll(".slider__dots-item").forEach(dot => {
-    dot.addEventListener("click", function() {
-      moveSlider(this.dataset.index);
-    })
-  })
-}
 
-function initTitle() {
-  images.forEach((image, index) => {
-    let title = `<li class="galary-item n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].title}</li>`;
-    sliderTitle.innerHTML += title;
-  });
-  sliderTitle.querySelectorAll(".galary-item").forEach(title => {
-    title.addEventListener("click", function() {
-      moveSlider(this.dataset.index);
-    })
-  })
-}
 
 function moveSlider(num) {
   sliderImages.querySelector(".active").classList.remove("active");
   sliderImages.querySelector(".n" + num).classList.add("active");
   sliderTitle.querySelector(".active").classList.remove("active");
   sliderTitle.querySelector(".n" + num).classList.add("active");
-  if (options.dots) {
-    sliderDots.querySelector(".active").classList.remove("active");
-    sliderDots.querySelector(".n" + num).classList.add("active");
-  }
+  sliderDots.querySelector(".active").classList.remove("active");
+  sliderDots.querySelector(".n" + num).classList.add("active");
 }
 
 function initAutoplay() {
@@ -102,7 +86,6 @@ function initAutoplay() {
 }
 
 let sliderOptions = {
-dots: true,
 autoplay: true,
 autoplayInterval: 5000
 };
